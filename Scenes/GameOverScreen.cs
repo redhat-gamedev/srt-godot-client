f628a140a98b95226bd2cccd58e31f626524ac50
Erik M Jacobs <erikmjacobs@gmail.com>
2023-01-24 10:06:03 -0500

    Updates starfield and changes game over
    * changes starfield parallax background image to our own
    * fixes starfield parallax behavior
    * adds game over graphic to game over screen
    * removes unused assets

--- Scenes/GameOverScreen.cs
+++ Scenes/GameOverScreen.cs
@@ -14,10 +14,10 @@ public class GameOverScreen : CanvasLayer
   // Called when the node enters the scene tree for the first time.
   public override void _Ready()
   {
-    MyGame = GetNode<Game>("/root/Game");
-    _serilogger = MyGame._serilogger;
+	MyGame = GetNode<Game>("/root/Game");
+	_serilogger = MyGame._serilogger;
 
-    ourGui = MyGame.GetNode<CanvasLayer>("GUI");
+	ourGui = MyGame.GetNode<CanvasLayer>("GUI");
   }
 
   //  // Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -28,22 +28,22 @@ public class GameOverScreen : CanvasLayer
 
   public void _on_TryAgainButton_button_up()
   {
-    // send the join message
-    bool success = MyGame.JoinGameAsPlayer(MyGame.myUuid);
-    if (!success)
-    {
-      _serilogger.Information($"GameOverScreen.cs: join failed TODO tell player why");
-      // TODO: alert errors or something 
-    }
-    else
-    {
-      // since we successfully joined the game, we can remove this node
-      // which is the game over screen. removing the screen "displays"
-      // the main game window
-      QueueFree();
+	// send the join message
+	bool success = MyGame.JoinGameAsPlayer(MyGame.myUuid);
+	if (!success)
+	{
+	  _serilogger.Information($"GameOverScreen.cs: join failed TODO tell player why");
+	  // TODO: alert errors or something 
+	}
+	else
+	{
+	  // since we successfully joined the game, we can remove this node
+	  // which is the game over screen. removing the screen "displays"
+	  // the main game window
+	  QueueFree();
 
-      // re-show the GUI
-      MyGame.initializeGameUI();
-    }
+	  // re-show the GUI
+	  MyGame.initializeGameUI();
+	}
   }
 }
